3969c4192c15c8eab49e1d48480f073406e0dc82
Maksim <maksim6erbinin@mail.ru>
2022-07-03 23:48:49 +0300

    Added task 19

---
+++ Task_19/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите пятизначное число: ");
+int userNum = Convert.ToInt16(Console.ReadLine());
+
+if(((userNum - userNum%10000)/10000 == userNum%10) && ((userNum%10000 - userNum%1000)/1000 == (userNum%100 - userNum%10)/10)) Console.WriteLine("число: " + userNum + " является полиндромом");
+else Console.WriteLine("число: " + userNum + " НЕ является полиндромом");
