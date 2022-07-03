b30b4423520123aec1862e4fc70819d8d2fdcb4e
Maksim <maksim6erbinin@mail.ru>
2022-07-04 02:51:39 +0300

    Added additional task

---
+++ Additional_task/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите номер искомого числа в последовательности круглых чисел: ");
+int userIndex = Convert.ToInt32(Console.ReadLine()) - 1;
+double binaryIndex = 0;
+
+for(double i = 0; userIndex >= 1; i++)
+{
+    binaryIndex = (userIndex % 2) * Math.Pow(10, i) + binaryIndex;
+    userIndex = userIndex / 2;
+}
+Console.WriteLine($"Искомое число: {binaryIndex*5}");
